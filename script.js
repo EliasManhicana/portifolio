a8b131eaa8b0b897b82aef3dfafe6f2e8a437bf9
Elias Manhicana <eliasmanhicane4@gmail.com>
2022-01-04 17:41:15 +0200

    Atualização da Largura do Site

--- script.js
+++ script.js
@@ -11,5 +11,6 @@ $(document).ready(function(){
     
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     });
 });
