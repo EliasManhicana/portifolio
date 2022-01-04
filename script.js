f11d2d8a42ffb1b42c86dd6c4ab397b7602c8e30
Elias Manhicana <eliasmanhicane4@gmail.com>
2022-01-04 15:27:40 +0200

    Início da Adição de Scripts JavaScript no Site

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-$()
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky")
+        } else {
+            $('.navbar').removeClass("sticky")
+        }
+    });
+
+    // script para mostar/esconder menu //
+    
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+    });
+});
