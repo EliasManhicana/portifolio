4a4bead08812bd8404e4006603ff9a5ba7c900aa
Elias Manhicana <eliasmanhicane4@gmail.com>
2022-01-24 17:52:23 +0200

    Fim da Estrutura do Site

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 $(document).ready(function(){
 
     $(window).scroll(function(){
+
         if (this.scrollY > 20) {
             $('.navbar').addClass("sticky")
         } else {
             $('.navbar').removeClass("sticky")
         }
+
+        if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show")
+        } else {
+            $('.scroll-up-btn').removeClass("show")
+        }
     });
+
+    // script de slide para cima //
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0}); 
+    });    
 
     // script para mostar/esconder menu //
     
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing animation script //
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Programador", "Designer", "Gestor de BD", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Programador", "Designer", "Gestor de BD", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     //owl carousel script //
