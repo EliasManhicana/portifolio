535dfa659fa80a0dc38f17d0b926f344fe9de97a
Elias Manhicana <eliasmanhicane4@gmail.com>
2022-01-24 15:19:38 +0200

    Conclusão da Secção Meus Projectos

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     $(window).scroll(function(){
         if (this.scrollY > 20) {
             $('.navbar').addClass("sticky")
@@ -13,4 +14,30 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    //owl carousel script //
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+
+            600:{
+                items: 2,
+                nav: false
+            },
+
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
 });
